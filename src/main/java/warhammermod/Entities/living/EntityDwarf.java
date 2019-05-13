9cec98bea8b89feda3c7181bcc930c42a97ee903
antoine <antio789@hotmail.fr>
2019-05-13 18:35:45 +0200

    1.8.3.3 added revenge mechanism to dwarfs with model axe/warhammer + shield or war pick added sound of grumbling when in combat increased lord spawn rate added shield blocking to dwarf

--- src/main/java/warhammermod/Entities/living/EntityDwarf.java
+++ src/main/java/warhammermod/Entities/living/EntityDwarf.java
@@ -1318,18 +1318,26 @@ public boolean attackEntityFrom(DamageSource source, float amount)
         return super.attackEntityFrom(source, amount);
 
 }
+    @Override
+    public boolean isActiveItemStackBlocking()
+    {
+        if(getProfession()!=1 && this.rand.nextInt(10)<4){
+            playSound(SoundEvents.ITEM_SHIELD_BLOCK,1,1);
+            return true;
+        }
+        else return false;
+    }
 
 
     public boolean canAttackClass(Class <? extends EntityLivingBase > cls)
     {
             return cls != EntityCreeper.class && super.canAttackClass(cls);
     }
-
+    /*
     public boolean attackEntityAsMob(Entity entityIn)
     {
         this.attackTimer = 7;
-        this.world.setEntityState(this, (byte)4);
-        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float)(10 + this.rand.nextInt(4)));
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float)(10 +getItemStackFromSlot(EntityEquipmentSlot.MAINHAND).getItemDamage()+ this.rand.nextInt(4)));
 
         if (flag)
         {
@@ -1339,6 +1347,61 @@ public boolean attackEntityFrom(DamageSource source, float amount)
         this.playSound(SoundEvents.ENTITY_PLAYER_ATTACK_STRONG, 1.0F, 1.0F);
         return flag;
     }
+    */
+    public boolean attackEntityAsMob(Entity entityIn)
+    {
+        float f = (float)this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+        int i = 0;
+
+        if (entityIn instanceof EntityLivingBase)
+        {
+            f += EnchantmentHelper.getModifierForCreature(this.getHeldItemMainhand(), ((EntityLivingBase)entityIn).getCreatureAttribute());
+            i += EnchantmentHelper.getKnockbackModifier(this);
+        }
+
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), f);
+
+        if (flag)
+        {
+            if (i > 0 && entityIn instanceof EntityLivingBase)
+            {
+                ((EntityLivingBase)entityIn).knockBack(this, (float)i * 0.5F, (double)MathHelper.sin(this.rotationYaw * 0.017453292F), (double)(-MathHelper.cos(this.rotationYaw * 0.017453292F)));
+                this.motionX *= 0.6D;
+                this.motionZ *= 0.6D;
+            }
+
+            int j = EnchantmentHelper.getFireAspectModifier(this);
+
+            if (j > 0)
+            {
+                entityIn.setFire(j * 4);
+            }
+
+            if (entityIn instanceof EntityPlayer)
+            {
+                EntityPlayer entityplayer = (EntityPlayer)entityIn;
+                ItemStack itemstack = this.getHeldItemMainhand();
+                ItemStack itemstack1 = entityplayer.isHandActive() ? entityplayer.getActiveItemStack() : ItemStack.EMPTY;
+
+                if (!itemstack.isEmpty() && !itemstack1.isEmpty() && itemstack.getItem().canDisableShield(itemstack, itemstack1, entityplayer, this) && itemstack1.getItem().isShield(itemstack1, entityplayer))
+                {
+                    float f1 = 0.25F + (float)EnchantmentHelper.getEfficiencyModifier(this) * 0.05F;
+
+                    if (this.rand.nextFloat() < f1)
+                    {
+                        entityplayer.getCooldownTracker().setCooldown(itemstack1.getItem(), 100);
+                        this.world.setEntityState(entityplayer, (byte)30);
+                    }
+                }
+            }
+
+            this.applyEnchantments(this, entityIn);
+        }
+        this.playSound(SoundEvents.ENTITY_PLAYER_ATTACK_STRONG, 1.0F, 1.0F);
+
+        return flag;
+    }
+
     private boolean testrandomsounddelay()
     {
 
